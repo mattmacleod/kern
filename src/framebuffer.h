d6d3de9015801310c4339afa63d950844a4395f9
Matthew MacLeod <matt@matt-m.co.uk>
2013-08-12 17:39:54 +0100

    Define VGA colours

--- src/framebuffer.h
+++ src/framebuffer.h
@@ -1,10 +1,25 @@
 #include "common.h"
 
-#define VGACOLOR_WHITE 15
-#define VGACOLOR_BLACK 0
-
 #define FRAMEBUFFER_COLS 80
 #define FRAMEBUFFER_ROWS 25
+
+#define VGACOLOR_BLACK   0
+#define VGACOLOR_BLUE    1
+#define VGACOLOR_GREEN   2
+#define VGACOLOR_CYAN    3
+#define VGACOLOR_RED     4
+#define VGACOLOR_MAGENTA 5
+#define VGACOLOR_YELLOW  6
+#define VGACOLOR_WHITE   7
+
+#define VGACOLOR_LIGHT_BLACK   8
+#define VGACOLOR_LIGHT_BLUE    9
+#define VGACOLOR_LIGHT_GREEN   10
+#define VGACOLOR_LIGHT_CYAN    11
+#define VGACOLOR_LIGHT_RED     12
+#define VGACOLOR_LIGHT_MAGENTA 13
+#define VGACOLOR_LIGHT_YELLOW  14
+#define VGACOLOR_LIGHT_WHITE   15
 
 void fb_clear();
 void fb_write_str(char *c);
--- src/main.c
+++ src/main.c
@@ -1,11 +1,5 @@
 int main(struct multiboot *mboot_ptr){
 	fb_clear();
 	fb_write_str("Hello, world!\n\n");
-	int i;
-	for( i=0; i<24; i++ ){
-		fb_set_colors(i%16, 0);
-		fb_write_int( i%16 );
-		fb_write_char('\n');
-	}
 	return 0xDEADBEEF;
 }
