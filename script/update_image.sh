90761a8d670043e80f8c7c7d91c0adda068a49b2
Matthew MacLeod <matt@umm.io>
2015-11-27 00:01:10 +0000

    Minimal implementation

--- script/update_image.sh
+++ script/update_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp `dirname $0`/../test/floppy.img /tmp/floppy.img
-sudo mount -o loop /tmp/floppy.img /mnt
+sudo cp `dirname $0`/../test/floppy.img tmp/floppy.img
+sudo mount -o loop tmp/floppy.img /mnt
 sudo cp `dirname $0`/../build/kernel /mnt/kernel
 sudo umount /mnt
--- src/main.c
+++ src/main.c
@@ -1,10 +1,10 @@
 int main(struct multiboot *mboot_ptr){
-	init_descriptor_tables();
-	fb_clear();
-	fb_write_str("Hello, world!\n\n");
+	//init_descriptor_tables();
+	// fb_clear();
+	// fb_write_str("Hello, world!\n\n");
 
-    asm volatile("int $0x3");
-    asm volatile("int $0x4");
+    // asm volatile("int $0x3");
+    // asm volatile("int $0x4");
     
-	return 0;
+	return 0xDEADBEEF;;
 }
